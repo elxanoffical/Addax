d51e317268e97419eef10cbe5e3f8ca7b22b22d7
Elxan <elxan.offical.07@mail.com>
2024-12-18 00:29:28 +0400

    addax project

---
+++ client/src/data/footer.js
@@ -0,0 +1,26 @@
+import { CiFacebook } from "react-icons/ci";
+import { TiSocialTwitterCircular } from "react-icons/ti";
+import { FaInstagram } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+
+
+export const footerlinks = [
+    {
+        img: FaInstagram,
+        href: '#'
+    },
+    {
+        img: CiFacebook,
+        href: '#'
+    },
+
+    {
+        img: TiSocialTwitterCircular,
+        href: '#'
+    }, 
+    {
+        img: CiLinkedin,
+        href: '#'
+    },
+   
+]
---
+++ client/src/data/navlinks.js
@@ -0,0 +1,23 @@
+export const navlinks = [
+    {
+        title: 'Ana sehife',
+        href: '#mainpage'
+    },
+    {
+        title: 'Komandamiz',
+        href: '#team'
+    },
+
+    {
+        title: 'Xidmetlerimiz',
+        href: '#services'
+    }, 
+    {
+        title: 'Bloq',
+        href: '#blog'
+    },
+    {
+        title: 'Elaqe',
+        href: '#contact'
+    },
+]
