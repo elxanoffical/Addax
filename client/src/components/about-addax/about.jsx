f6c8b525afb9c4b7c6adbb26701e2b03a3151e90
Elxan <elxan.offical.07@gmail.com>
2025-03-04 16:20:10 +0400

    about component

---
+++ client/src/components/about-addax/about.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import './about.scss'
+import { FaCheck } from "react-icons/fa6";
+import video from "../../assets/image/about.jpg";
+
+const About = () => {
+  return (
+      <div className="about-addax">
+           <div className="container">
+             <div className="section-title">
+               <div className="liner">
+                 <div id="line1"></div>
+                 <div id="line2"></div>
+               </div>
+               <div className="txt">
+                 <h1>
+                   About <span>Addax</span>
+                 </h1>
+                 <p>
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                   eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 </p>
+               </div>
+             </div>
+   
+             <div className="marketing-video">
+               <div className="marketing-wrapper">
+                 <div className="top-txt">
+                   <h2>
+                     We Are Digital <span>Marketing & Branding</span>
+                   </h2>
+                   <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                     do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                   </p>
+                 </div>
+   
+                 <div className="bottom-wrapper">
+                   <div className="txt-wrapper">
+                     <div className="txt">
+                       <FaCheck className="icon" />
+                       <p>Creative Design</p>
+                     </div>
+                     <div className="txt">
+                       <FaCheck className="icon" />
+                       <p>Retina Ready</p>
+                     </div>
+                     <div className="txt">
+                       <FaCheck className="icon" />
+                       <p>Responsive Design</p>
+                     </div>
+                   </div>
+                   <div className="txt-wrapper">
+                     <div className="txt">
+                       <FaCheck className="icon" />
+                       <p>Modern Design</p>
+                     </div>
+                     <div className="txt">
+                       <FaCheck className="icon" />
+                       <p>Awesome Design</p>
+                     </div>
+                     <div className="txt">
+                       <FaCheck className="icon" />
+                       <p>Digital Marketing & Branding</p>
+                     </div>
+                   </div>
+                 </div>
+                 <button>Read More</button>
+               </div>
+               <div className="video">
+                 <img src={video} alt="" />
+               </div>
+             </div>
+           </div>
+         </div>
+   
+  )
+}
+
+export default About
--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -2,92 +2,21 @@ import React from "react";
 import "./Home.scss";
 import { footerlinks } from "../../data/footer";
 
-import { FaCheck } from "react-icons/fa6";
-import video from "../../assets/image/about.jpg";
 import stragety from "../../assets/image/stragety.jpg";
 import Header from "../../components/header/header";
 import Hero from "../../components/hero/hero";
 import Welcome from "../../components/welcome-addax/welcome";
+import About from "../../components/about-addax/about";
 
 const Home = () => {
   return (
     <div className="section">
-       
-       <Header/>
-       
-       <Hero/>
-     
-      <Welcome/>
-      
+      <Header />
 
-      <div className="about-addax">
-        <div className="container">
-          <div className="section-title">
-            <div className="liner">
-              <div id="line1"></div>
-              <div id="line2"></div>
-            </div>
-            <div className="txt">
-              <h1>
-                About <span>Addax</span>
-              </h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
+      <Hero />
 
-          <div className="marketing-video">
-            <div className="marketing-wrapper">
-              <div className="top-txt">
-                <h2>
-                  We Are Digital <span>Marketing & Branding</span>
-                </h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-
-              <div className="bottom-wrapper">
-                <div className="txt-wrapper">
-                  <div className="txt">
-                    <FaCheck className="icon" />
-                    <p>Creative Design</p>
-                  </div>
-                  <div className="txt">
-                    <FaCheck className="icon" />
-                    <p>Retina Ready</p>
-                  </div>
-                  <div className="txt">
-                    <FaCheck className="icon" />
-                    <p>Responsive Design</p>
-                  </div>
-                </div>
-                <div className="txt-wrapper">
-                  <div className="txt">
-                    <FaCheck className="icon" />
-                    <p>Modern Design</p>
-                  </div>
-                  <div className="txt">
-                    <FaCheck className="icon" />
-                    <p>Awesome Design</p>
-                  </div>
-                  <div className="txt">
-                    <FaCheck className="icon" />
-                    <p>Digital Marketing & Branding</p>
-                  </div>
-                </div>
-              </div>
-              <button>Read More</button>
-            </div>
-            <div className="video">
-              <img src={video} alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Welcome />
+      <About />
 
       <div className="strategy-addax">
         <div className="container">
